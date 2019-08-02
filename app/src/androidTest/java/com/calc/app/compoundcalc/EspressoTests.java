d6dbaa9e2edb0f3ea3b2981e2d75ec164cc5557e
jfassbender3 <jfassbender3@gatech.edu>
2019-08-01 17:06:22 -0700

    setup test files

---
+++ app/src/androidTest/java/com/calc/app/compoundcalc/EspressoTests.java
@@ -0,0 +1,21 @@
+package com.calc.app.compoundcalc;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class EspressoTests {
+}
